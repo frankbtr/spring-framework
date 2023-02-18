275b86f102de12ee4118572e1e6650a458602fd5
Frank Batur <frank_batur@outlook.com>
2023-02-18 15:42:54 -0500

    Stereotype Annotation

---
+++ Spring-01-Core-IoC/src/main/java/com/frank/stereotype_annotation/JavaClass.java
@@ -0,0 +1,10 @@
+package com.frank.stereotype_annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JavaClass {
+    public void getTeachingHours(){
+        System.out.println("Total teaching hours: 250");
+    }
+}
