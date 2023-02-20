cf9d68492c90b335b8b63738132ba39530158855
Frank Batur <frank_batur@outlook.com>
2023-02-20 17:19:20 -0500

    Loosely coupled

---
+++ Spring-00-Coupling/src/main/java/com/frank/loosely_coupled/App.java
@@ -0,0 +1,14 @@
+package com.frank.loosely_coupled;
+
+public class App {
+    public static void main(String[] args) {
+
+        FullTimeMentor fullTime = new FullTimeMentor();
+        //PartTimeMentor partTime = new PartTimeMentor();
+
+        MentorAccount mentor = new MentorAccount(fullTime);
+
+        mentor.manageAccount();
+
+    }
+}
