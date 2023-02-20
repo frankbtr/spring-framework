0de0e21afe418715c8f13972cb624c041ad0d214
Frank Batur <frank_batur@outlook.com>
2023-02-20 10:58:12 -0500

    Wiring

--- Spring-01-Core-IoC/src/main/java/com/frank/stereotype_annotation/App.java
+++ Spring-01-Core-IoC/src/main/java/com/frank/stereotype_annotation/App.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-
         ApplicationContext context = new AnnotationConfigApplicationContext(ConfigCourse.class);
+        context.getBean(JavaClass.class).getTeachingHours();
+        context.getBean(Agile.class).getTeachingHours();
+        //context.getBean(Selenium.class).getTeachingHours(); //We did not put @Component on Selenium class
     }
 }
--- Spring-01-Core-IoC/src/main/java/com/frank/stereotype_annotation/Selenium.java
+++ Spring-01-Core-IoC/src/main/java/com/frank/stereotype_annotation/Selenium.java
@@ -2,7 +2,7 @@ package com.frank.stereotype_annotation;
 
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Selenium {
     public void getTeachingHours(){
         System.out.println("Total teaching hours: 200");
