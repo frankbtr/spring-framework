b8e8332ecc4e568fffbc5f9249ed0b190a6fb91b
Frank Batur <frank_batur@outlook.com>
2023-03-05 00:37:33 -0500

    PathVariable

--- Spring-08-Spring-MVC-Model/src/main/java/com/frank/controller/CarController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/frank/controller/CarController.java
@@ -2,6 +2,7 @@ package com.frank.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,6 +23,13 @@ public class CarController {
 
         model.addAttribute("make", make);
 //        model.addAttribute("year", year);
+        return "car/car-info";
+    }
+
+    @RequestMapping("/info/{make}/{year}")  //localhost:8080/car/info/honda
+    public String getCarInfo(@PathVariable String make, @PathVariable Integer year, Model model){
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
         return "car/car-info";
     }
 }
