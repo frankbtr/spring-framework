fdd472579589621ac147964cdbee899492fe9b31
Frank Batur <frank_batur@outlook.com>
2023-03-19 23:32:37 -0400

    linked two pages and iteration

--- Spring-09-Thymelef/src/main/java/com/frank/controller/StudentController.java
+++ Spring-09-Thymelef/src/main/java/com/frank/controller/StudentController.java
@@ -12,8 +12,12 @@ public class StudentController {
     @RequestMapping("/register")
     public String register(Model model){
         model.addAttribute("students", DataGenerator.createStudent());
-
         return "student/register";
+    }
+
+    @RequestMapping("/welcome")
+    public String info(){
+        return "student/welcome";
     }
 
 }
