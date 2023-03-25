a6d6b3d9bee2f43210087ba8f3e06e35ab99e183
Frank Batur <frank_batur@outlook.com>
2023-03-25 17:08:26 -0400

    Spring Thymeleaf

--- Spring-09-Thymelef/src/main/java/com/frank/controller/MentorController.java
+++ Spring-09-Thymelef/src/main/java/com/frank/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.frank.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -19,5 +21,12 @@ public class MentorController {
         List<String> batchList = Arrays.asList("JD1", "JD2" ,"JD3", "JD4", "JD5");
         model.addAttribute("batchList", batchList);
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String showForm2(@ModelAttribute("mentor")  Mentor mentor, Model model){
+        System.out.println(mentor.toString());
+       // return "mentor/mentor-confirmation";
+        return "redirect:/mentor/register";
     }
 }
