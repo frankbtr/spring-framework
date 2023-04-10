8b32fd7a07f7f65cbea3237fd8efe6815a32263a
Frank Batur <frank_batur@outlook.com>
2023-04-09 22:28:41 -0400

    some annotations practice

--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into EMPLOYEES(id,name)
-values (1,'Mike Smith');
+-- insert into EMPLOYEES(id,name)
+-- values (1,'Mike Smith');
 
 insert into STUDENT(id, first_name, last_name, email)
-values (1, 'Frank', 'Batur', 'frank.batur@outlook.com');
+values ('Frank', 'Batur', 'frank.batur@outlook.com');
