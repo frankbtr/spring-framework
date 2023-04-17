e49beffeb61a3cd02ace91c7310d03d535fc8abf
Frank Batur <frank_batur@outlook.com>
2023-04-16 22:07:26 -0400

    Added Employee and Department

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/frank/repository/DepartmentRepository.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/frank/repository/DepartmentRepository.java
@@ -2,6 +2,8 @@ package com.frank.repository;
 
 import com.frank.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 }
--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/frank/repository/EmployeeRepository.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/frank/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.frank.repository;
 
 import com.frank.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
