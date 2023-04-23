a1b76280e85d692ee892cc0b28838aa668980268
Frank Batur <frank_batur@outlook.com>
2023-04-22 22:49:35 -0400

    flyway migration

--- Spring-13-ORM-CinemaLab/src/main/java/com/frank/entity/Ticket.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/frank/entity/Ticket.java
@@ -2,27 +2,26 @@ package com.frank.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class MovieCinema extends BaseEntity{
+public class Ticket extends BaseEntity{
 
+    private Integer seatNumber;
+    private Integer rowNumber;
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime dateTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private Movie movie;
+    private MovieCinema movieCinema;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private Cinema cinema;
-
+    private User userAccount;
 }
