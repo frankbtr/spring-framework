1e01b4002f318489e22f3d4fe92dc67da85c81fc
Frank Batur <frank_batur@outlook.com>
2023-04-28 23:07:26 -0400

    Queries wip

--- Spring-14-ORM-Queries/src/main/java/com/frank/bootstrap/DataGenerator.java
+++ Spring-14-ORM-Queries/src/main/java/com/frank/bootstrap/DataGenerator.java
@@ -1,5 +1,6 @@
 package com.frank.bootstrap;
 
+import com.frank.repository.CourseRepository;
 import com.frank.repository.DepartmentRepository;
 import com.frank.repository.EmployeeRepository;
 import com.frank.repository.RegionRepository;
@@ -12,11 +13,13 @@ public class DataGenerator implements CommandLineRunner {
     private final RegionRepository regionRepository;
     private final DepartmentRepository departmentRepository;
     private final EmployeeRepository employeeRepository;
+    private final CourseRepository courseRepository;
 
-    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
+    public DataGenerator(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository, CourseRepository courseRepository) {
         this.regionRepository = regionRepository;
         this.departmentRepository = departmentRepository;
         this.employeeRepository = employeeRepository;
+        this.courseRepository = courseRepository;
     }
 
     @Override
@@ -38,13 +41,28 @@ public class DataGenerator implements CommandLineRunner {
         System.out.println("---------------------Department End--------------");
 
 
-        System.out.println("------------------Employee Start --------------");
+        System.out.println("------------------Employee Start --------------------");
         //jhookd@booking.com
         System.out.println("findByEmployee: " + employeeRepository.getEmployeeDetail());
         System.out.println("findByEmployee: " + employeeRepository.getEmployeeSalary());
-
-
         System.out.println("----------------------Employee End-------------------");
+
+
+
+        System.out.println("------------------Course Start --------------");
+        courseRepository.findByCategory("Spring").forEach(System.out::println);
+        System.out.println("---------------");
+        courseRepository.findByCategoryOrderByName("Spring").forEach(System.out::println);
+        System.out.println("---------------");
+        System.out.println(courseRepository.existsByName("JavaScript for all"));
+        System.out.println("---------------");
+        System.out.println(courseRepository.countByCategory("Spring"));
+        System.out.println("---------------");
+        courseRepository.findByNameStartsWith("Scalable").forEach(System.out::println);
+        System.out.println("---------------");
+        courseRepository.streamByCategory("Spring").forEach(System.out::println);
+        System.out.println("------------------Course End --------------");
+
 
     }
 }
--- Spring-14-ORM-Queries/src/main/java/com/frank/repository/CourseRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/frank/repository/CourseRepository.java
@@ -3,8 +3,29 @@ package com.frank.repository;
 import com.frank.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
     //find all courses by category
+    List<Course> findByCategory(String category);
+
+    //find all courser by category and order the entities by name
+    List<Course> findByCategoryOrderByName(String category);
+
+    //checks if a course with the provided name exists, return true of course exists, false otherwise
+    boolean existsByName(String name);
+
+    //returns the count of courses for the provided category
+    int countByCategory(String category);
+
+    //find all courses that start with the provided name
+    List<Course> findByNameStartsWith(String name);
+
+    //find all courses by category and returns a stream
+    Stream<Course> streamByCategory(String category);
+
+
 
 }
