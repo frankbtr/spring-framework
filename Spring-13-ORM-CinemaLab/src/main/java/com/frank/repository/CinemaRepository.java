1ad0fab262c0ef5c0c7e2cb6f0f5487323699b2d
Frank Batur <frank_batur@outlook.com>
2023-04-30 21:45:43 -0400

    CinemaRepository started

--- Spring-13-ORM-CinemaLab/src/main/java/com/frank/repository/CinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/frank/repository/CinemaRepository.java
@@ -2,25 +2,28 @@ package com.frank.repository;
 
 import com.frank.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
 
     // ------------------- DERIVED QUERIES ------------------- //
     //TASK 6 min
     //Write a derived query to get cinema with a specific name
-
+    Optional<Cinema> findByName(String name);
 
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
-
-
+    List<Cinema> findFirst3BySponsoredNameContainingOrderBySponsoredName(String sponsoredName);
 
     //Write a derived query to list all cinemas in a specific country
-
-
+    List<Cinema> findAllByLocationCountry(String country);
 
     //Write a derived query to list all cinemas with a specific name or sponsored name
-
+    List<Cinema> findAllByNameOrSponsoredName(String name, String sponsoredName);
 
 
     // ------------------- JPQL QUERIES ------------------- //
