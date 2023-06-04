556d296f7e035f16d971013c49b6999852d0a433
Frank Batur <frank_batur@outlook.com>
2023-06-03 20:39:39 -0400

    added controller

---
+++ Spring-18-REST-OpenApi3/src/main/java/com/frank/repository/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.frank.repository;
+
+import com.frank.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+}
