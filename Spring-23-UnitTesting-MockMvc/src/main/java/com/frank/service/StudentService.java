310bc672e9fcefe8f996045019d6b30d2e95865e
Frank Batur <frank_batur@outlook.com>
2023-07-03 11:44:12 -0400

    WelcomeControllerTest added

---
+++ Spring-23-UnitTesting-MockMvc/src/main/java/com/frank/service/StudentService.java
@@ -0,0 +1,23 @@
+package com.frank.service;
+
+import com.frank.entity.Student;
+import com.frank.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudent() {
+        return studentRepository.findAll();
+    }
+
+}
+
